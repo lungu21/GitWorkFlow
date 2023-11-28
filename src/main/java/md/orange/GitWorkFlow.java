b4540ac37a44c255996d5296d058e15795b7e4a5
lungu01 <vlada.01.lungu@gmail.com>
2023-11-28 23:10:25 +0200

    Define the git workflow for future development strategy

--- src/main/java/md/orange/GitWorkFlow.java
+++ src/main/java/md/orange/GitWorkFlow.java
@@ -1,3 +1,5 @@
+package md.orange;
+
 public class GitWorkFlow {
     public static void main(String[] args) {
         System.out.println("Vlada");
---
+++ src/main/java/md/orange/gitworkflow/GitWorkflow2.java
@@ -0,0 +1,8 @@
+package md.orange.gitworkflow;
+
+public class GitWorkflow2 {
+    public static void main(String[] args) {
+        System.out.println("Vreau acasa");
+        System.out.println("Vreau acasa tare");
+    }
+}
